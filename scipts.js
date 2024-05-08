3f008ae9bfcdeee16519ae8684f16deef7cf2e73
jaingaurav1601 <jaingauravgj16@gmail.com>
2024-05-08 15:03:15 +0530

    Initiallized

--- scipts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Example: Show an alert when the contact form is submitted
-    const contactForm = document.querySelector('#contact form');
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent form from submitting
-        alert('Thank you for your message!');
-    });
-});
